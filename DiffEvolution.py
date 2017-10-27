91cb4bc5a47c59eeaed36f220d3ba1cf26e1d1ff
neilswalton <31712025+neilswalton@users.noreply.github.com>
2017-10-27 12:24:28 -0600

    Implemented mutate() and completed evolve()

--- DiffEvolution.py
+++ DiffEvolution.py
@@ -8,8 +8,8 @@ class DiffEvolution(Darwin):
 	#def __init__(self, beta):
 	#	self.beta = beta
 
-	def create_instance(beta, population_size, nodes_per_layer):
-		obj = DiffEvolution(population_size, nodes_per_layer)
+	def create_instance(beta, population_size, nodes_per_layer, activation_function, problem_type):
+		obj = DiffEvolution(population_size, nodes_per_layer, activation_function, problem_type)
 		obj.beta = beta
 		return obj 
 
@@ -17,23 +17,24 @@ class DiffEvolution(Darwin):
 		''' Given three individuals, create the trial vector, u
 			to cross with another individual '''
 
-		print ("Mutating...")
-		trial_vector = individuals[0]
-		return trial_vector
+		ind1 = np.array(individuals[0])
+		ind2 = np.array(individuals[1])
+		ind3 = np.array(individuals[2])
+
+		trial_vector = np.add(ind1, (self.beta * np.subtract(ind2, ind3)))
+
+		return trial_vector.tolist()
 
 	def select_parents(self):
 		''' Select 4 random parents for mutating and crossover'''
 
 		random.shuffle(self.population)
 		parents = self.population[0:4]
-		print ("PARENTS: " + str(parents))
 		return parents
 
 	def evolve(self):
 		''' Combine selection, mutation, crossover, and replacement
 			to evolve the population into its next generation '''
-
-		print ("Evolving...")
 
 		new_pop = []
 
@@ -43,15 +44,17 @@ class DiffEvolution(Darwin):
 			parents = self.select_parents()
 			parent1 = parents[0]
 			parent2 = self.mutate(parents[1:])
-			print("parent 1: " + str(parent1) + "   parent 2: " + str(parent2))
 			offspring = self.crossover(parent1, parent2)
-			new_pop.append(offspring1)
+			new_pop.append(offspring)
 			#new_pop.append(offspring2)
 
 		self.replace(new_pop, "fittest")
 
 
 if __name__ == '__main__':
-	test = DiffEvolution.create_instance(0.2, 5, [2, 3, 1])
-	for i,ind in enumerate(test.population):
-		print ("Individual " + str(i) + ": " + str(ind))
+	test = DiffEvolution.create_instance(0.1, 4, [2, 2, 1], "sigmoid", "classification")
+	#for i,ind in enumerate(test.population):
+	#	print ("Individual " + str(i) + ": " + str(ind))
+	net = test.create_mlp(test.population[0])
+	for layer in net.layers:
+		print (layer.weights)
