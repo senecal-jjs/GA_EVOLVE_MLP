cf829eb9c259581378bdcb9c266d8597426c9b20
sdilts <stuart.dilts@gmail.com>
2017-11-02 20:14:54 -0600

    Added Individual named tuple, _create_population

--- MuLambdaES.py
+++ MuLambdaES.py
@@ -1,4 +1,10 @@
+import numpy as np
+from collections import namedtuple
+
+
 class MuLambda(Darwin):
+
+    Individual = namedtuple('Individual', genes, sigmas)
 
     def create_instance(mu, lamb, pop_size, nodes_per_layer, actFunc, problem_type):
         obj = MuLambda(pop_size, nodes_per_layer, actFunc, problem_type)
@@ -12,7 +18,13 @@ class MuLambda(Darwin):
 
     # need to overload this to get paired sigma values:
     def _create_population(self):
-        pass
+        pop = []
+        items = super._create_population()
+        for i in items:
+            # add the sigma values
+            sigmas = np.random.uniform(0,1, len(i))
+            pop.append(Individual(genes=i,sigmas=sigmas))
+        return pop
 
     def select_parents(self):
         raise "Don't call this method, silly"
