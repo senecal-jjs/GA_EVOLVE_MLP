b839489570242a3a40e55e916a16c18129ec2037
sdilts <stuart.dilts@gmail.com>
2017-11-02 22:54:28 -0600

    Fully implemented Local variance adaptation
    + Has not been tested yet.
    + Default initial sigma values may be a little too big.

--- MuLambdaES.py
+++ MuLambdaES.py
@@ -15,12 +15,8 @@ class MuLambda(Darwin):
         # r values chosen based off of Back and Schwefel (1993)
         obj.r1 = 1/math.sqrt(2 * len(obj.population[0]))
         obj.r2 = 1/math.sqrt(2 * math.sqrt(len(obj.population[0])))
-
+        obj.t = 0
         return obj
-
-    def mutate(self, individuals):
-        "Return one mutated individual"
-        pass
 
     # need to overload this to get paired sigma values:
     def _create_population(self):
@@ -28,24 +24,45 @@ class MuLambda(Darwin):
         items = super._create_population()
         for i in items:
             # add the sigma values
-            sigmas = np.random.uniform(0,1, len(i))
+            # initial sigma suggested to be ~3.0 (Thomas Back, 1996)
+            sigmas = np.ones(len(i)) * 3.0
             pop.append(MuLambda.Individual(genes=i,sigmas=sigmas))
         return pop
-
-    def select_parents(self):
-        raise "Don't call this method, silly"
 
     def _loc_new_sigma(indiv):
         return indiv.sigmas * np.exp(u_zero * self.r1 + self.r2 * np.random.normal(0,1))
 
-    def _local_es(self):
+    def mutate(self, individual):
+        "Returns a new individual that has been mutated based on the given individual"
+        new_genes = []
+        new_sigmas = self._loc_new_sigma(indiv)
+        for g, sig in zip(individual.genes, new_sigmas):
+            # no ceiling or floor for our weights:
+            new_genes.append(g + np.random.normal(0, sig))
+        return Individual(sigmas=new_sigmas, genes=new_genes)
+
+    def _pick_top(n, canidates, validation_data):
+        pool = []
+        for indiv in canidates:
+            pool.append((indiv, self.fitness(indiv, validation_data)))
+        pool.sort(key=itemgetter(1))
+        return [ind[0] for ind in pool[:n-1:-1] ]
+
+    def local_es(self, validation_data):
+        new_pop = self.population.copy()
+        for indiv in np.random.choice(self.population, self.lamb, replace=True):
+            new_pop.append(self.mutate(indiv))
+        self.population = self._pick_top(self.population_size, new_pop, validation_data)
+
+    def global_se(self, validation_data):
         pass
 
-    def _global_se(self):
-        pass
-
-    def evolve(self, variance_type):
+    def evolve(self, variance_type, validation_data):
         if variance_type == "local":
-            self._local_es()
+            self.local_es(validation_data)
         else:
-            self._global_se()
+            self.global_se(validation_data)
+        t = t + 1
+
+    def select_parents(self):
+        raise "Don't call this method, silly"
